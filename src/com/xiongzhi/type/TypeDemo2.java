8e2a85b379972275c312f418a84bfbdcb3a60255
BeaRzZzZ <47800627+BeaRzZzZ@users.noreply.github.com>
2022-04-03 23:01:04 +0100

    Day1

--- src/com/xiongzhi/type/TypeDemo2.java
+++ src/com/xiongzhi/type/TypeDemo2.java
@@ -7,12 +7,12 @@ public class TypeDemo2 {
         byte b = (byte)a; /*byte的范围是[-128,127]，没有超出这个范围的强转没问题，
                         超出这个范围就会强制取后面的几位，根据强转后的类型定，第一位是符号位*/
 
-        //强转导致的数据溢出
+        //  强转导致的数据溢出
         int a1 = 1500;
         byte b1 = (byte)a1;//b1=-36
         System.out.println(b1);
 
-        //浮点强转整型会直接丢掉小数部分，保留整数部分
+        //  浮点强转整型会直接丢掉小数部分，保留整数部分
         double score = 99.5;
         int it = (int)score;
         System.out.println(it);//it = 99

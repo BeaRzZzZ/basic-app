7aee6eda201421f2c4248df0248a8422ed9bcdc6
BeaRzZzZ <47800627+BeaRzZzZ@users.noreply.github.com>
2022-04-17 18:14:53 +0100

    Day10

--- src/com/xiongzhi/_8_String/StringBasic.java
+++ src/com/xiongzhi/_8_String/StringBasic.java
@@ -3,6 +3,8 @@ package com.xiongzhi._8_String;
 public class StringBasic {
     public static void main(String[] args) {
         // String对象是不可变字符串，这里每次修改都指向了堆内存中的新的字符串对象（每次拼接新生成了一个String对象）
+        // 为什么不可变? 因为如果可变，那常量池就没意义了。用双引号创建的两个相同内容会指向堆内存中的同一个对象，这样子设计的目的
+        // 是为了减少内存资源的占用，但是如果能修改，那么修改其中一个就会影响另外一个。并且这样设计是线程安全的
         String name = "点对点"; // 左边是推荐方式，还可以通过构造器生成
         name+="教育";
         name+="集团";
